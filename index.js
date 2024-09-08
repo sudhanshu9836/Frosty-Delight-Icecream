4b6ec772fb13efedf3211f2913c3007091e7bea3
Sudhanshu Tiwari <sudhanshutiwari9836@gmail.com>
2024-09-09 02:34:21 +0530

    Final changes done

--- index.js
+++ index.js
@@ -149,7 +149,7 @@ app.delete("/bag/:id",(req, res)=>{
     let id = req.params.id;
     orders = orders.filter( (p)=> id!==p.id);
     res.redirect("/bag")
-})
+}) 
 app.post("/ordered", (req,res)=>{
     const username = req.body.name;
     const userNo = req.body.contactNumber;
